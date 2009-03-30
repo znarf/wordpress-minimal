96a7f013a681dbfee0403e57cf30c66be15fcca7
François Hodierne <francois@upian.com>
2009-03-30 13:09:27 +0200

    remove useless div

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,15 @@
-<div>
+<div id="sidebar">
 
-	<div id="sidebar">
+	<div id="primary" class="sidebar">
+		<ul class="xoxo">
+			<?php dynamic_sidebar(1) ?>
+		</ul>
+	</div><!-- #primary .sidebar -->
 
-		<div id="primary" class="sidebar">
-			<ul class="xoxo">
-				<?php dynamic_sidebar(1) ?>
-			</ul>
-		</div><!-- #primary .sidebar -->
-
-		<div id="secondary" class="sidebar">
-			<ul class="xoxo">
-				<?php dynamic_sidebar(2) ?>
-			</ul>
-		</div><!-- #secondary .sidebar -->
-
-	</div>
+	<div id="secondary" class="sidebar">
+		<ul class="xoxo">
+			<?php dynamic_sidebar(2) ?>
+		</ul>
+	</div><!-- #secondary .sidebar -->
 
 </div>
