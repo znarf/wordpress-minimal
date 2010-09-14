51e2f975794d68b7df0787499055ab631a80bd6a
François Hodierne <francois@hodierne.net>
2010-09-14 11:44:21 +0200

    fix deprecated syntax

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 
 	<div id="header">
 		<h1 class="h6e-page-title" id="blog-title"><span>
-			<a href="<?php bloginfo('home') ?>/" title="<?php echo wp_specialchars( get_bloginfo('name'), 1 ) ?>" rel="home">
+			<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 				<?php bloginfo('name') ?>
 			</a>
 		</span></h1>
